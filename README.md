bb974683d5085c15470e9ba662f6d44485392e01
ezhang7423 <ezhang7423@student.palomar.edu>
2021-07-06 14:32:30 -0700

    done

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # stats
+
+grad school stats from gradcafe and https://github.com/jjdelvalle/gradcafe_analysis
+
+
+Admission statistics for Stanford University
+
+2019: 15.38% acceptance rate, 130 applicants
+2018: 14.29% acceptance rate, 63 applicants
+2017: 25.0% acceptance rate, 36 applicants
+
+
+
+
+Admission statistics for Carnegie Mellon University
+
+2019: 17.29% acceptance rate, 133 applicants
+2018: 40.0% acceptance rate, 20 applicants
+2017: 50.0% acceptance rate, 16 applicants
+
+
+
+
+Admission statistics for Massachusetts Institute Of Technology
+
+2019: 15.0% acceptance rate, 100 applicants
+2018: 16.67% acceptance rate, 18 applicants
+2017: 33.33% acceptance rate, 21 applicants
+
+
+
+
+Admission statistics for Berkeley
+
+2019: 19.79% acceptance rate, 96 applicants
+2018: 33.33% acceptance rate, 63 applicants
+2017: 34.33% acceptance rate, 67 applicants
+
+
+
+
+Admission statistics for Alberta
+
+2019: 100.0% acceptance rate, 1 applicants
+2018: 28.57% acceptance rate, 7 applicants
+2017: 0.0% acceptance rate, 2 applicants
+
+
+
+
+Admission statistics for Cambridge
+
+2019: 23.53% acceptance rate, 17 applicants
+2018: 83.33% acceptance rate, 6 applicants
+2017: 50.0% acceptance rate, 2 applicants
+
+
+
+
+* This dataset is not representative of the entire population. It suffers from selection bias, and true acceptance rates are likely much lower
---
+++ gen.py
@@ -0,0 +1,56 @@
+import pandas as pd
+
+
+full = pd.read_csv("cs.csv", encoding="ISO-8859-1")
+full = full[full[" Degree type and admission semester"].str.contains("PhD", na=False)]
+
+
+def get_acceptance(stan):
+    try:
+        return round(
+            (
+                len(stan[stan[" Acception/rejection"] == "Accepted"].index)
+                / len(stan.index)
+            )
+            * 100,
+            2,
+        )
+    except ZeroDivisionError:
+        return None
+
+
+def get_school(school, full):
+    return full[full["Institution"].str.contains(school, na=False)]
+
+
+def get_yr(yr, full):
+    return full[full[" Date posted"].str.contains(yr, na=False)]
+
+
+interesting_years = ["2019", "2018", "2017"]
+interesting_schools = [
+    "Stanford University",
+    "Carnegie Mellon University",
+    "Massachusetts Institute Of Technology",
+    "Berkeley",
+    "Alberta",
+    "Cambridge",
+]
+
+for school in interesting_schools:
+    print(f"Admission statistics for {school}\n")
+    sel_school = get_school(school, full)
+    for yr in interesting_years:
+        ds = get_yr(yr, sel_school)
+
+        if not (
+            acceptance := f"{get_acceptance(ds)}% acceptance rate, {len(ds.index)} applicants"
+        ):
+            acceptance = "No data"
+
+        print(f"{yr}: {acceptance}")
+    print("\n\n\n")
+
+print(
+    "* This dataset is not representative of the entire population. It suffers from selection bias, and true acceptance rates are likely much lower"
+)
